351798936b3bb68bc24751020add3dede49e2ec3
stupachek <alona.stupak@gmail.com>
2022-10-19 22:21:55 +0300

    edit main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include "calculator.h"
-using namespace std;
 
 int main() {
-    auto calculator = new Calculator();
-    cout << "2.5 + 8.5 = "<< calculator->Add(2.5,8.5)<< endl;
-    cout << "8.5 - 2.5 = "<< calculator->Sub(8.5,2.5)<< endl;
+    Calculator calculator{};
+    std::cout << "2.5 + 8.5 = " << calculator.Add(2.5,8.5) << '\n';
+    std::cout << "8.5 - 2.5 = " << calculator.Sub(8.5,2.5) << '\n';
     return 0;
 }
